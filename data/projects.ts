d9354ba7de0ef456564b7ea9ef16cf3b6f49c013
Bhuvan Sen <bhuvansen4@gmail.com>
2026-01-22 09:02:19 +0530

    Changes

--- data/projects.ts
+++ data/projects.ts
@@ -1,28 +1,22 @@
-import TcsLogo from "../Assets/Projects/TcsLogo.svg"
 import ShoeSite from "../Assets/Projects/ShoeSite.jpg"
-import PortfolioSite from "../Assets/Projects/PortfolioSite.jpg"
+import PortfolioSite from "../Assets/Projects/PortfolioSite.png"
+import ExpenseSharing from "../Assets/Projects/ExpenseSharing.png"
 
 export const projectList = [
-    {
-        name: "A6 Modernization",
-        description:
-            "It's a corporate project made while working for TCS for Scotiabank for onboarding their customers.",
-        logoImage: TcsLogo,
-    },
-    {
-        name: "DigitalCore",
-        description:
-            "It's a corporate project made while working for TCS for Bank of Montreal. It's a migration project, where backend had been migrated from TIBCO Java to AWS Lambda functions using Node JS.",
-        logoImage: TcsLogo,
-    },
     {
         name: "E-Commerce",
         description: "It's a personal project made for selling shoes using MERN Stack",
         logoImage: ShoeSite,
     },
     {
+        name: "Expense Sharing App",
+        description:
+            "It's a full stack web application made for sharing expenses among friends using Spring Boot and Next",
+        logoImage: ExpenseSharing,
+    },
+    {
         name: "Portfolio",
-        description: "It's a personal portfolio websote made for showcasing my work",
+        description: "It's a personal portfolio website made for showcasing my work",
         logoImage: PortfolioSite,
     },
 ]
